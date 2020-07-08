e60f535bbb7f07bf2a6530b6657341d939bb7458
leesojin <lsjsj92@naver.com>
2020-07-08 20:48:53 +0900

    first test

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='soojin_lib',
+    version='0.0.1',
+    description='soojin pip install test',
+    url='git@github.com:lsjsj92/git_pip_install.git',
+    author='leesoojin',
+    author_email='asfd',
+    license='soojin',
+    packages=['soojin_lib'],
+    zip_safe=False,
+    install_requires=[
+        'numpy==1.18.3'
+    ]
+)
---
+++ soojin_lib/soojin.py
@@ -0,0 +1,2 @@
+def print_soojin(msg):
+    print(f"안녕하세요? 이수진입니다. {msg}")
